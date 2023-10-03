1270641410ab00f40b85f42cc2b35393c2deee7c
Dinkar Kumar <dinkarkumar.god@gmail.com>
2023-10-04 00:17:21 +0530

    updated project

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,9 @@
+// Import necessary React components and hooks
 import React, { useContext, useState } from 'react';
 import { useParams } from 'react-router';
 import { UserContext } from '../App';
+
+// Import components used in the Dashboard
 import AddAdmin from '../Components/Dashboard/AddAdmin/AddAdmin';
 import AddServices from '../Components/Dashboard/AddService/AddServices';
 import AllAdmin from '../Components/Dashboard/AllAdmin/AllAdmin';
@@ -13,30 +16,37 @@ import OrderList from '../Components/Dashboard/OrderList/OrderList';
 import Profile from '../Components/Dashboard/Profile/Profile';
 import Review from '../Components/Dashboard/Review/Review';
 import SideBar from '../Components/Dashboard/SideBar/SideBar';
+
+// Import styles for the Dashboard
 import './Dashboard.css';
 
-
-const Dashboard = ({adminLoading}) => {
+// Define the Dashboard component
+const Dashboard = ({ adminLoading }) => {
     const { isAdmin } = useContext(UserContext);
-    const {panel}=useParams()
+    const { panel } = useParams();
     const [showSidebar, setShowSidebar] = useState(false);
 
     return (
         <main className="dashboard-container">
-          <SideBar show={showSidebar} adminLoading={adminLoading} />
-            <div  id="content">
-               <NavBar setShowSidebar={setShowSidebar} show={showSidebar}  />
+            {/* Display the sidebar component */}
+            <SideBar show={showSidebar} adminLoading={adminLoading} />
+
+            <div id="content">
+                {/* Display the navigation bar component */}
+                <NavBar setShowSidebar={setShowSidebar} show={showSidebar} />
+
+                {/* Render different components based on the 'panel' parameter */}
                 {
-                    panel === "profile" ?  <Profile />
-                    :panel === 'book' ? <Book />
-                    :panel === "book-list" ? <BookList />
-                    :panel === "reviews" ? <Review />
-                    :panel === "add-services" && isAdmin ?  <AddServices />
-                    :panel === "add-admins" && isAdmin  ? <AddAdmin />
-                    :panel === "all-orders" && isAdmin  ? <OrderList />
-                    :panel === "manage-services" && isAdmin  ? <ManageService />
-                    :panel === "all-reviews" && isAdmin  ? <AllReview />
-                    :panel === "all-admins" && isAdmin  ? <AllAdmin />
+                    panel === "profile" ? <Profile />
+                    : panel === 'book' ? <Book />
+                    : panel === "book-list" ? <BookList />
+                    : panel === "reviews" ? <Review />
+                    : panel === "add-services" && isAdmin ? <AddServices />
+                    : panel === "add-admins" && isAdmin ? <AddAdmin />
+                    : panel === "all-orders" && isAdmin ? <OrderList />
+                    : panel === "manage-services" && isAdmin ? <ManageService />
+                    : panel === "all-reviews" && isAdmin ? <AllReview />
+                    : panel === "all-admins" && isAdmin ? <AllAdmin />
                     : null
                 }
             </div>
