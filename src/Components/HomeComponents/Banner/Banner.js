e25ab195e387f06a09c39330b0fae9197c430afb
DINKAR202 <Dinkar202@outlook.com>
2023-10-16 16:20:15 +0530

    upload commit

--- src/Components/HomeComponents/Banner/Banner.js
+++ src/Components/HomeComponents/Banner/Banner.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
-import { Col, Container, Image, Row } from "react-bootstrap";
+// import { Col, Container, Image, Row } from "react-bootstrap";
 import banner from "../../../images/banner-im.jpg";
 import "./Banner.css";
 
@@ -12,36 +12,77 @@ const Banner = () => {
       transition={{ delay: 0.5, type: "spring", stiffness: 2500 }}
       id="home"
     >
-      <Container className="border">
-        <Row className="align-items-center justify-content-center banner">
-          <Col md={4}>
-            <h1>We're Crafting Solutions for Your Challenges.</h1>
-            <p className="text-muted">
-              <small>
-                Our motorcycle repair technicians pride themselves on great
-                skill in diagnosing and fixing a wide range of problems. With
-                years of hands-on experience, they excel at troubleshooting and
-                solving various motorcycle-related challenges to ensure your car
-                runs flawlessly.
-              </small>
-            </p>
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                textShadow: "0px 0px 8px rgb(255 255 255)",
-                boxShadow: "0px 0px 8px #17a2b8",
-                transition: { duration: 0.4, yoyo: "Infinity" },
-              }}
-              className="main-button"
-            >
-              Get Started
-            </motion.button>
-          </Col>
-          <Col className="offset-md-1" md={6}>
-            <Image className="img-fluid" src={banner} alt="..." />
-          </Col>
-        </Row>
-      </Container>
+      <div id="carouselExampleCaptions" className="carousel slide">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={0}
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={1}
+            aria-label="Slide 2"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={2}
+            aria-label="Slide 3"
+          />
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={banner} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={banner} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={banner} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </motion.section>
   );
 };
